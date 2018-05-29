99d4edd7473ba96e559bfa5a33ee012f2120d682
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-05-28 23:19:26 -0400

    updated

--- package.bat
+++ package.bat
@@ -1,10 +1,8 @@
-set function_name=awk-lnkchk-start
+set function_name=aws-lnkchk-start
 
-
-call C:\Users\18589\Envs\pylambda\Scripts\activate
+REM Zip the lambda function
 call del /q lambda_function.zip
 call "c:\Program Files\7-Zip\7z.exe" a lambda_function.zip *.py
-
 
 REM Upload the new code
 call aws lambda update-function-code --function-name %function_name% --zip-file fileb://lambda_function.zip
---
+++ test/sync_s3_link-checker.bat
@@ -0,0 +1,9 @@
+
+set timestamp=%date:~10,4%-%date:~4,2%-%date:~7,2%-%time:~0,2%%time:~3,2%%time:~6,2%
+REM Replace spaces with underscores
+set timestamp=%timestamp: =_%
+
+echo Download
+echo http://www.nerdthoughts.net > ".\s3_link-checker\%timestamp%.txt"
+echo http://www.nerdthoughts.net/about >> ".\s3_link-checker\%timestamp%.txt"
+call aws s3 sync .\s3_link-checker\ s3://link-checker/ 
