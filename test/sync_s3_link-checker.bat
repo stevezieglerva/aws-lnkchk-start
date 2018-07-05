eafb96fd6d13cebcaa199783f9887d8f5b240048
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-07-05 07:11:41 -0400

    Update link sync

--- test/sync_s3_link-checker.bat
+++ test/sync_s3_link-checker.bat
@@ -3,10 +3,10 @@ set timestamp=%date:~10,4%-%date:~4,2%-%date:~7,2%-%time:~0,2%%time:~3,2%%time:~
 REM Replace spaces with underscores
 set timestamp=%timestamp: =_%
 
-echo Download
+
 copy /y test_links.txt ".\s3_link-checker\%timestamp%.txt"
 call aws s3 sync .\s3_link-checker\ s3://link-checker/ 
 
-
+type test_links.txt
 
 cd
